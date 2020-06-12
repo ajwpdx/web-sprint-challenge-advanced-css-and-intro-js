1227eff6e3c9759f6fc5b051c972a457c06c5126
Alex Williams <alexjwilliams1@gmail.com>
2020-06-12 10:13:53 -0700

    did strecth 2

--- index.js
+++ index.js
@@ -346,11 +346,20 @@ function getHTML(/* Code here */){
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
 
-    /* Code here */
 
+function randomize(array){
+    let j, x, i
+    for (i=array.length-1; i>0; i--){
+      j = Math.floor(Math.random()* (i+1));
+      x = array[i];
+      array[i] = array[j]
+      array[j] = x
+    }
+    return array;
+    //used the Fisher-Yates suffle agorithm
   }
 
+  console.log(randomize(artists))
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
